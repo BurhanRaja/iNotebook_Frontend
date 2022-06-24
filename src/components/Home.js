69364b61d67eda23920a401938ab76c70447f168
BurhanuddinRaja <mails4burhan@gmail.com>
2022-06-24 16:21:14 +0530

    fetching the data manually and improve UI

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext'
 
 const Home = () => {
+    const tags = ["Work", "Personal", "School", "Travel"]
+    const context = useContext(noteContext)
+    const {notes, setNotes} = context
     return (
         <div>
-            <h1>This is iNotebook</h1>
+            <div className='container my-5'>
+                <h1>Add Your Note</h1>
+                <form className='my-3'>
+                    <div className="form-floating">
+                        <input type="text" className="form-control" id="floatingInput" placeholder="title" style={{ "borderBottom": "none", "borderBottomLeftRadius": "1px", "borderBottomRightRadius": "1px" }} />
+                        <label for="floatingInput">Title</label>
+                    </div>
+                    <div className="form-floating">
+                        <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea" style={{ "height": "200px", "borderTop": "none", "borderTopLeftRadius": "1px", "borderTopRightRadius": "1px" }}></textarea>
+                        <label for="floatingTextarea">Desciption Here</label>
+                    </div>
+                    <h4 className='my-3'>Tags</h4>
+                    <div className='d-flex'>
+                        {tags.map((tag) => {
+                            return <div className="form-check mx-2">
+                                <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                                <label className="form-check-label" for="flexCheckDefault">
+                                    {tag}
+                                </label>
+                            </div>
+                        })}
+                        <div className="form-check mx-2 d-flex">
+                            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                            <label for="inputAddress" className="form-label mx-2 my-0">Other:</label>
+                            <input type="text" className="form-control py-1" id="inputAddress" placeholder="eg: Food" style={{ "width": "80%" }} />
+                        </div>
+                    </div>
+                </form>
+
+                <h2 className='my-5'>Your Current Notes</h2>
+                {notes.map((note) => {
+                    return note.title
+                })}
+            </div>
         </div>
     )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,9 +1,32 @@
+import React, { useState } from 'react'
 import noteContext from "./noteContext"
-import React from 'react'
 
 const NoteState = (props) => {
+  const noteInitial = [
+    {
+      "_id": "62b4003c06626cd58010d00c",
+      "user": "62b345c707d3953626308440",
+      "title": "My title",
+      "description": "Please I have to wake up early and study",
+      "tags": "selfimprovement",
+      "date": "2022-06-23T05:55:08.292Z",
+      "__v": 0
+    },
+    {
+      "_id": "62b400c806626cd58010d01a",
+      "user": "62b345c707d3953626308440",
+      "title": "My Work title",
+      "description": "I have to work till 8:00 PM",
+      "tags": "work",
+      "date": "2022-06-23T05:57:28.831Z",
+      "__v": 0
+    }
+  ]
+
+  const [notes, setnotes] = useState(noteInitial)
+
   return (
-    <noteContext.Provider value={{"state":"state"}}>
+    <noteContext.Provider value={{notes, setnotes}}>
       {props.children}
     </noteContext.Provider>
   )
